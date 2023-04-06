363933fcf63053cc2c78d96ec41041188720cf66
askarrizz <askar.minaimbek@fizmat.kz>
2023-04-06 15:13:20 +0600

    lsn10_getiles

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -2,13 +2,14 @@ const fileService = require('../services/fileService')
 const User = require('../models/User')
 const File = require('../models/File')
 
+
 class FileController {
     async createDir(req, res) {
         try {
             const {name, type, parent} = req.body
             const file = new File({name, type, parent, user: req.user.id})
             const parentFile = await File.findOne({_id: parent})
-            if (!parentFile) {
+            if(!parentFile) {
                 file.path = name
                 await fileService.createDir(file)
             } else {
@@ -25,6 +26,18 @@ class FileController {
         }
     }
 
+    async getFiles(req, res) {
+        try {
+            const files = await File.find({user: req.user.id, parent: req.query.parent})
+            return res.json(files)
+        } catch (e) {
+            console.log(e)
+            return res.status(500).json({message: "Can not get files"})
+        }
+    }
+}
+
+module.exports = new FileController()
     async fetFiles(req, res) {
         try {
             const files = await File.find({user: req.user.id, parent: req.query.parent})
